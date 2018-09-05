82e4ebaba47c90f36a971eb19ab72aeb38d1642f
Xavi Torelló <info@xaviertorello.cat>
2018-09-05 14:27:42 +0200

    Provide default action states constants generator

--- app/containers/App/actions.js
+++ app/containers/App/actions.js
@@ -57,3 +57,14 @@ export function repoLoadingError(error) {
     error,
   };
 }
+
+const REQUEST = 'REQUEST';
+const SUCCESS = 'SUCCESS';
+const FAILURE = 'FAILURE';
+
+export function createActionTypes(base) {
+  return [REQUEST, SUCCESS, FAILURE].reduce((acc, type) => {
+    acc[type] = `${base}_${type}`;
+    return acc;
+  }, {});
+}
