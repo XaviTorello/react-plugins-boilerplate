a0c7c8f9a46f2ef20fa7934fb4091b965d69700d
Xavi Torelló <info@xaviertorello.cat>
2018-09-05 18:23:27 +0200

    Prepare login reducers
    if success, set the username, the token and disable loading
    
    if requested, activate loading
    
    if failure, prepare the error and disable loading

--- app/containers/LoginPage/reducer.js
+++ app/containers/LoginPage/reducer.js
@@ -1,5 +1,5 @@
 /*
- * HomeReducer
+ * loginReducer
  *
  * The reducer takes care of our data. Using actions, we can change our
  * application state.
@@ -11,22 +11,50 @@
  */
 import { fromJS } from 'immutable';
 
+import { createActionTypes } from '../App/actions';
+
 import { CHANGE_USERNAME } from './constants';
+
+export const LOGIN = createActionTypes('LOGIN');
 
 // The initial state of the App
 export const initialState = fromJS({
   username: false,
   token: false,
+  loading: false,
 });
 
-function homeReducer(state = initialState, action) {
+function loginReducer(state = initialState, action) {
+  console.log('Reducing action', action);
   switch (action.type) {
     case CHANGE_USERNAME:
       // Delete prefixed '@' from the github username
       return state.set('username', action.name.replace(/@/gi, ''));
+
+    case LOGIN.REQUEST:
+      return state
+        .set('username', false)
+        .set('token', false)
+        .set('loading', true)
+        .set('error', false);
+
+    case LOGIN.SUCCESS:
+      return state
+        .set('username', 'rolf')
+        .set('token', 'th1sIs4Tok3n!!!')
+        .set('loading', false)
+        .set('error', false);
+
+    case LOGIN.FAILURE:
+      return state
+        .set('username', false)
+        .set('token', false)
+        .set('loading', false)
+        .set('error', action.error.message || true);
+
     default:
       return state;
   }
 }
 
-export default homeReducer;
+export default loginReducer;
