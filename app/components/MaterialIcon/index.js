87ce8e3517a22313c019ee746dd05b74b7cc1d6d
Xavi Torelló <info@xaviertorello.cat>
2018-09-17 18:43:16 +0200

    Fix MaterialIcon

--- app/components/MaterialIcon/index.js
+++ app/components/MaterialIcon/index.js
@@ -2,8 +2,9 @@
  * A material-ui icon rendered dynamically
  */
 
-import React from 'react';
 // import styled from 'styled-components';
 import * as Icons from '@material-ui/icons';
 
-export default (MaterialIcon = icon => Icons[icon]);
+const MaterialIcon = icon => Icons[icon];
+
+export default MaterialIcon;
