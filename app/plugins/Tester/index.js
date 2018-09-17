201a929ebfdb844dcc44d8378f325e7b288358af
Xavi Torelló <info@xaviertorello.cat>
2018-09-17 18:12:53 +0200

    Provide some interesting plugins test

--- app/plugins/Tester/index.js
+++ app/plugins/Tester/index.js
@@ -1,12 +1,42 @@
 import React from 'react';
+import { Switch, Route } from 'react-router';
+
+const divTest = (
+  <div>
+    <h1>This is a test!!!!</h1>
+  </div>
+);
+
+class Tester1 extends React.Component {
+  render = () => {
+    console.log('1TESTER');
+    return (
+      <div>
+        <h1>This is a 111!!!!</h1>
+      </div>
+    );
+  };
+}
+
+class Tester2 extends React.Component {
+  render = () => {
+    console.log('2TESTER');
+    return (
+      <div>
+        <h2>This is a test2222!!!!</h2>
+      </div>
+    );
+  };
+}
 
 export default class Tester extends React.Component {
-
-    render() {
-      return (
-          <div>
-              <h1>This is a test!!!!</h1>
-          </div>
-      )
-    }
+  render() {
+    console.log('TESTER');
+    return (
+      <Switch>
+        <Route exact path="/component/1" component={Tester1} />
+        <Route exact path="/rolf/2" component={Tester2} />
+      </Switch>
+    );
+  }
 }
--- app/plugins/index.js
+++ app/plugins/index.js
@@ -1,28 +1,12 @@
-import React from 'react'
-import { Switch, Route} from 'react-router'
+import React from 'react';
+import { Switch, Route } from 'react-router';
 
-import Tester from './Tester'
-
-const Testezr = (props) => {
-    return (
-        <div>
-            <h1>This is a test!</h1>
-        </div>
-    )
-}
-
-const Tester2 = (props) => {
-    return (
-        <div>
-            <h1>This is NOT a test!</h1>
-        </div>
-    )
-}
+import Tester from './Tester';
 
 const Plugins = () => (
-    <Switch>
-        <Route exact path="/component" component={Tester}/>
-    </Switch>
-)
+  <Switch>
+    <Route component={Tester} />
+  </Switch>
+);
 
-export default Plugins
+export default Plugins;
