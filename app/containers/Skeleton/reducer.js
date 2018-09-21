a590e362dd86c15f470cf619144a846d2d92b59b
Xavi Torelló <info@xaviertorello.cat>
2018-09-21 11:59:19 +0200

    Add MENU reducers

--- app/containers/Skeleton/reducer.js
+++ app/containers/Skeleton/reducer.js
@@ -8,6 +8,8 @@ import { fromJS } from 'immutable';
 
 import { CHANGE_MENU } from './constants';
 
+import { MENU } from './actions';
+
 import * as initial from './initialState';
 
 export const initialState = fromJS({
@@ -18,6 +20,25 @@ function menuReducer(state = initialState, action) {
   switch (action.type) {
     case CHANGE_MENU:
       return state.set('menu', action.menu);
+
+    /* ///////////
+    //  MENU!  //
+    /////////// */
+
+    case MENU.REQUEST:
+      return state.set('loading', true).set('error', false);
+
+    case MENU.SUCCESS:
+      return state
+        .set('menu', fromJS(action.menu))
+        .set('loading', false)
+        .set('error', false);
+
+    case MENU.FAILURE:
+      return state
+        .set('loading', false)
+        .set('error', action.error.message || true);
+
     default:
       return state;
   }
