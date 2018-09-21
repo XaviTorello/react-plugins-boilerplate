7fd4c6c413058b2feb9bee1e946c7a58abf745b4
Xavi Torelló <info@xaviertorello.cat>
2018-09-21 12:05:44 +0200

    Inject reducers and saga to Skeleton
    Skeleton now fetch the menu to render the entries based on current
    logged user!

--- app/containers/Skeleton/index.js
+++ app/containers/Skeleton/index.js
@@ -9,6 +9,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { compose } from 'redux';
 import { createSelector } from 'reselect';
 import Header from 'components/Header';
 
@@ -20,10 +21,22 @@ import Divider from '@material-ui/core/Divider';
 import MaterialIcon from 'components/MaterialIcon';
 import Link from 'components/Link';
 
+import injectReducer from 'utils/injectReducer';
+import injectSaga from 'utils/injectSaga';
+
 import { makeSelectMenu } from './selectors';
+
+import * as actions from './actions';
+import reducer from './reducer';
+import saga from './saga';
 
 export class HeaderMenu extends React.PureComponent {
   // eslint-disable-line react/prefer-stateless-function
+
+  componentDidMount() {
+    // Load the menu on load page!
+    this.props.loadMenu();
+  }
   render() {
     const menuFormatted = this.props.menu.map(entry => {
       const EntryIcon =
@@ -59,10 +72,31 @@ export class HeaderMenu extends React.PureComponent {
 HeaderMenu.propTypes = {
   menu: PropTypes.object.isRequired,
   children: PropTypes.object.isRequired,
+  loadMenu: PropTypes.func,
 };
+
+export function mapDispatchToProps(dispatch) {
+  return {
+    loadMenu: evt => {
+      if (evt !== undefined && evt.preventDefault) evt.preventDefault();
+      dispatch(actions.menu.request('a_username'));
+    },
+  };
+}
 
 const mapStateToProps = createSelector(makeSelectMenu(), menu => ({
   menu,
 }));
 
-export default connect(mapStateToProps)(HeaderMenu);
+const withConnect = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+);
+const withReducer = injectReducer({ key: 'menu', reducer });
+const withSaga = injectSaga({ key: 'menu', saga });
+
+export default compose(
+  withReducer,
+  withSaga,
+  withConnect,
+)(HeaderMenu);
